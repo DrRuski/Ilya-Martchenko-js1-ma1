69a7370a17e20eda120266c5de1def24d9d6e806
DrRuski <ilia_e@hotmail.com>
2022-06-19 10:36:11 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+// Question 1
+
+const cat = {
+    complain: function(){
+        console.log("Meow!")
+    }
+};
+cat.complain();
+    
+
+
+//Question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated Heading";
+
+
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+
+
+// Question 4
+
+
+
+
+
+// Question 5
+
+const paragraph = document.querySelector("p");
+
+paragraph.style.color = "red";
+
+
+
+
+// Question 8
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
